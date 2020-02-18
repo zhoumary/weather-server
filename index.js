f93ee9f50d2425b95f9ed7a8a3b86e2ebad87cba
I323799 <fiona.zhou01@sap.com>
2020-02-18 14:36:03 +0800

    enhance the post api for weather

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -20,13 +21,18 @@ Connection.connect(err => {
 console.log(Connection);
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 app.get('/', (req, res) => {
     res.send('Hello from the weather server!');
 });
 
 
-app.get('/weather', (req, res) => {
+app.get('/weathers', (req, res) => {
     Connection.query(SELECT_ALL_WEATHER, (err, weather) => {
         if (err) {
             return res.send(err);
@@ -38,7 +44,29 @@ app.get('/weather', (req, res) => {
     });
 });
 
-app.get('/weather/add', (req, res) => {
+
+app.get('/weathers/:id', (req, res) => {
+    let weather_id = req.params.id;
+    if (!weather_id) {
+        return res.status(400).send({
+            error: true,
+            message: "Please provide valid weather id!"
+        });
+    }
+
+    Connection.query('SELECT * FROM weather_list_table where id=?', weather_id, (err, results, fields) => {
+        if (err) {
+            throw err;
+        }
+        return res.send({
+            error: false,
+            data: results[0],
+            message: 'weather instance'
+        });
+    })
+})
+
+app.get('/weathers/add', (req, res) => {
     const { time, city, temparature } = req.query;
     const INSERT_WEATHER_QUERY = `INSERT INTO weather_list_table(time, city, temparature) VALUES('${time}', '${city}', '${temparature}')`;
     Connection.query(INSERT_WEATHER_QUERY, (err, results) => {
@@ -51,11 +79,44 @@ app.get('/weather/add', (req, res) => {
 });
 
 
-app.post('/weather/create', (req, res) => {
-
+app.post('/weather', (req, res) => {
+    let { time, city, temparature } = req.body;
+    if (!city || !time) {
+        return res.status(400).send({
+            error: true,
+            message: "Please provide weather data!"
+        })
+    }
+    const INSERT_WEATHER = `INSERT INTO weather_list_table SET ?`;
+    Connection.query(INSERT_WEATHER, {
+        time: time,
+        city: city,
+        temparature: temparature
+    }, (error, results, fields) => {
+        if (error) {
+            throw error;
+        }
+        return res.send({
+            error: false,
+            data: results,
+            message: "New weather has been created!"
+        })
+    })
 });
+
+
+app.put('/weather/:id', (req, res) => {
+    let { time, city, temparature } = req.body;
+
+})
 
 
 app.listen(4000, () => {
     console.log('Weather Server listening on port 4000');
 });
+
+module.exports = app;
+
+
+
+
